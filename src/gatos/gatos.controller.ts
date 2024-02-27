96c64fe96a65bfa882ec2b76971ab262b1ad1bc1
MarleyDouglas007 <marley.santos2308@gmail.com>
2024-02-27 16:02:12 -0300

    criação dos gatos e aparecer eles

--- src/gatos/gatos.controller.ts
+++ src/gatos/gatos.controller.ts
@@ -1,17 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import {Gato} from 'src/gato/gato.interface';
 
 @Controller('gatos')
 export class GatosController {
 
 
- private readonly gatos: Gato[] = [];
+ private readonly gatos: Gato[] = [
+   {
+      id:1,
+      nome: "Black",
+      raca: "Mestiça",
+      idade: 2
+  },
+
+   {
+      id:2,
+      nome: "Garfield",
+      raca: "Desconhecida",
+      idade: 45
+   }
+ ];
 
  @Get()
  findAll(): Gato[]{
     return this.gatos;
 
  }
+ @Get(':id')
+ findOne(@Param('id') id:string): Gato | undefined{   
+   return this.gatos.find((gato)=> gato.id == Number(id))
 
-
+}
 }
