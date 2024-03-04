86d5ee00eebe9a8165632e9076b8f6be8cb1772f
MarleyDouglas007 <marley.santos2308@gmail.com>
2024-03-04 13:38:28 -0300

    atual 04

--- src/gatos/gatos.controller.ts
+++ src/gatos/gatos.controller.ts
@@ -1,34 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import {Gato} from 'src/gato/gato.interface';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { Gato } from 'src/gato/gato.interface';
 
 @Controller('gatos')
 export class GatosController {
 
+    private readonly gatos: Gato[] = [
+        {
+            id: 1,
+            nome: "Black",
+            raca: "Mestiça",
+            idade: 2
+        },
+        {
+            id: 2,
+            nome: "Garfield",
+            raca: "Desconhecida",
+            idade: 45
+        }
+    ];
 
- private readonly gatos: Gato[] = [
-   {
-      id:1,
-      nome: "Black",
-      raca: "Mestiça",
-      idade: 2
-  },
+    @Get()
+    findAll(): Gato[] {
+        return this.gatos;
+    }
 
-   {
-      id:2,
-      nome: "Garfield",
-      raca: "Desconhecida",
-      idade: 45
-   }
- ];
+    @Get(':id')
+    findOne(@Param('id') id : string, @Res() response: Response) {
+        const gato = this.gatos.find((gato) => gato.id === Number(id));
+        if(gato){
+            response.status(HttpStatus.OK).json(gato);
+        } else {
+            response.status(HttpStatus.NOT_FOUND).send();
+        }
+    }
 
- @Get()
- findAll(): Gato[]{
-    return this.gatos;
-
- }
- @Get(':id')
- findOne(@Param('id') id:string): Gato | undefined{   
-   return this.gatos.find((gato)=> gato.id == Number(id))
-
-}
 }
